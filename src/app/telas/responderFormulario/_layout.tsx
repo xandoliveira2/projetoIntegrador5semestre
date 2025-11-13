873f8bffb224699d75f4a44393880d098bb62058
GuilhermePaulinoRibeiro <guilhermepaulinoribeiro@gmail.com>
2025-11-13 08:04:07 -0300

    Corrigido caminho da pasta app configurado em package.json

--- src/app/telas/responderFormulario/_layout.tsx
+++ src/app/telas/responderFormulario/_layout.tsx
@@ -12,8 +12,17 @@ export default function Layout() {
       screenOptions={{
         headerTitle: "Responder Formulário",
         headerShadowVisible: false,
-        headerStyle: { backgroundColor: "#fff" },
-        headerTitleStyle: { fontWeight: "bold" },
+        headerStyle: {
+          backgroundColor: "#fff",
+          height: 60, // ⬅️ diminui a altura (padrão é ~90 em alguns dispositivos)
+        },
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 18, // opcional, pode ajustar o tamanho da fonte também
+        },
+        headerLeftContainerStyle: {
+          paddingLeft: 10, // evita que o botão fique muito colado na borda
+        },
         headerLeft: () => (
           <TouchableOpacity onPress={() => router.back()}>
             <Ionicons name="chevron-back" size={26} color="#000" />
